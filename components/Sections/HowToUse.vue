568474ea21ad4c6712eb9e62dbc4d5dd2056aaeb
Sagit Gutiérrez <jozaguts@hotmail.com>
2020-06-13 00:53:38 -0500

    add how to use component

---
+++ components/Sections/HowToUse.vue
@@ -0,0 +1,149 @@
+<template>
+  <section class="ss_section_five ss_section_sec_bg spacer_top spacer_bottom">
+    <!--===== Section Five Start =====-->
+    <div class="container">
+      <h3 class="ss_h3_center text-center">Hay muchas ventajas en un sistema de paneles solares</h3>
+      <h1 class="text-center mb-5 mt-3">Beneficios</h1>
+      <div class="row">
+        <div class="col-xl-4 col-lg-6">
+          <div class="ss_four_left">
+            <div class="ss_box_bg wow fadeIn" data-wow-delay="0.1s" data-wow-duration="1s">
+              <div class="row">
+                <div class="col-3">
+                  <div class="ss_four_one">
+                    <img
+                      class="img-fluid"
+                      src="@/assets/images/howtouse/invertion.svg"
+                      alt="iamgen Buena inversion "
+                    />
+                  </div>
+                </div>
+                <div class="col-9">
+                  <div class="ss_four_two">
+                    <h2>Buena inversión</h2>
+                    <p>Tus paneles solares se pagan solos Una buena inversión en ahorro de energía representa una autentica protección contra aumentos inflacionarios de la economía y precios de energía</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="ss_box_bg mt-4 wow fadeIn" data-wow-delay="0.1s" data-wow-duration="1s">
+              <div class="row">
+                <div class="col-3">
+                  <div class="ss_four_one">
+                    <img
+                      class="img-fluid"
+                      src="@/assets/images/howtouse/pollution.svg"
+                      alt="Imagen Reduces contaminacion"
+                    />
+                  </div>
+                </div>
+                <div class="col-9">
+                  <div class="ss_four_two">
+                    <h2>Reduces la contaminación</h2>
+                    <p>Reduces tu huella de carbón, ayudas a tu comunidad y a tu planeta.</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="ss_box_bg mt-4 wow fadeIn" data-wow-delay="0.1s" data-wow-duration="1s">
+              <div class="row">
+                <div class="col-3">
+                  <div class="ss_four_one">
+                    <img
+                      class="img-fluid"
+                      src="@/assets/images/howtouse/plusvalia.svg"
+                      alt="imagen plusvalia"
+                    />
+                  </div>
+                </div>
+                <div class="col-9">
+                  <div class="ss_four_two">
+                    <h2>Plusvalía a tu propiedad</h2>
+                    <p>Instalar estos sistemas le dan plusvalía a tu propiedad para venta o renta porque puedes ofrecer una propiedad que tiene un consumo mínimo de energía.</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-xl-4 col-lg-6 order-xl-last mt-lg-0 mt-md-4">
+          <div class="ss_four_left">
+            <div class="ss_box_bg wow fadeIn" data-wow-delay="0.3s" data-wow-duration="1s">
+              <div class="row">
+                <div class="col-3">
+                  <div class="ss_four_one">
+                    <img
+                      class="img-fluid"
+                      src="@/assets/images/services/soportetecnico.svg"
+                      alt="imagen soporte técnico"
+                    />
+                  </div>
+                </div>
+                <div class="col-9">
+                  <div class="ss_four_two">
+                    <h2>Servicio, mantenimiento y garantía</h2>
+                    <p>Ya instalados los Paneles Solares no generarán gastos posteriores ya que no utilizan agua, gasolina o algún tipo de combustible. Y no te preocupes, no producen ningún tipo de ruido o molestia.</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="ss_box_bg mt-4 wow fadeIn" data-wow-delay="0.3s" data-wow-duration="1s">
+              <div class="row">
+                <div class="col-3">
+                  <div class="ss_four_one">
+                    <img
+                      class="img-fluid"
+                      src="@/assets/images/howtouse/invertion.svg"
+                      alt="imagn ahorro dinero"
+                    />
+                  </div>
+                </div>
+                <div class="col-9">
+                  <div class="ss_four_two">
+                    <h2>Ahorro de por vida</h2>
+                    <p>Durante la vida de tu equipo tus ahorros podrán ser desde cientos de miles hasta millones de pesos.</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="ss_box_bg mt-4 wow fadeIn" data-wow-delay="0.3s" data-wow-duration="1s">
+              <div class="row">
+                <div class="col-3">
+                  <div class="ss_four_one">
+                    <img
+                      class="img-fluid"
+                      src="@/assets/images/howtouse/invertion.svg"
+                      alt="imagen reducen impuestos"
+                    />
+                  </div>
+                </div>
+                <div class="col-9">
+                  <div class="ss_four_two">
+                    <h2>Deduce impuestos, incentivos fiscales</h2>
+                    <p>El Gobierno Federal permite deducir fiscalmente el 100% de la inversión el primer año. La inversión se reduce hasta en un 30%.</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-xl-4 offset-xl-0 col-lg-6 offset-lg-3">
+          <div class="ss_easy_use wow zoomIn" data-wow-delay="0.2s" data-wow-duration="1s">
+            <img
+              class="img-fluid"
+              src="@/assets/images/howtouse/panelsolarintalation.jpg"
+              alt="Instalcion de paneles solares"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
