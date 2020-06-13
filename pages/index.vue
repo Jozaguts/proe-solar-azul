7284277dab1cc2272b00c5ad2ba8afd82b7fdd5a
Sagit Gutiérrez <jozaguts@hotmail.com>
2020-06-13 00:56:12 -0500

    add home

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,28 @@
 <template>
-  <h1>index</h1>
+  <div>
+    <HeaderAndMainBanner />
+    <ServicesCardComponent />
+    <SolarPanelInfo />
+    <HowToUse />
+    <FooterComponent />
+  </div>
 </template>
 
 <script>
-export default {}
+import HeaderAndMainBanner from '~/components/Base/HeaderAndMainBanner'
+import ServicesCardComponent from '~/components/Sections/ServicesCard'
+import FooterComponent from '~/components/Sections/Footer'
+import SolarPanelInfo from '~/components/Sections/SolarPanelInfo'
+import HowToUse from '~/components/Sections/HowToUse'
+export default {
+  components: {
+    HeaderAndMainBanner,
+    ServicesCardComponent,
+    SolarPanelInfo,
+    HowToUse,
+    FooterComponent
+  }
+}
 </script>
 
 <style>
