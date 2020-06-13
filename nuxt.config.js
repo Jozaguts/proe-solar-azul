292cbaffcaf571ccedf86210e126b0433fc78680
Sagit Gutiérrez <jozaguts@hotmail.com>
2020-06-13 00:42:57 -0500

    add vue-scrollto and owl-carousel

--- nuxt.config.js
+++ nuxt.config.js
@@ -35,6 +35,7 @@ module.exports = {
   ** Plugins to load before mounting the App
   */
   plugins: [
+    { src: 'plugins/owl-carousel.js', ssr: false }
   ],
   /*
   ** Nuxt.js dev-modules
@@ -47,6 +48,7 @@ module.exports = {
   modules: [
     // Doc: https://axios.nuxtjs.org/usage
     '@nuxtjs/axios',
+    'vue-scrollto/nuxt',
   ],
   /*
   ** Axios module configuration
