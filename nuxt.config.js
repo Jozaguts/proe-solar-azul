00ca58bc8777ca1a38cee0694d264279f505f641
Sagit Gutiérrez <jozaguts@hotmail.com>
2020-06-14 15:09:53 -0500

    test deploy 3

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,7 +5,9 @@ const routerBase = process.env.DEPLOY_ENV === 'GH_PAGES' ? {
 } : {}
 
 module.exports = {
-  ...routerBase,
+  router: {
+    base: '/proe-solar-azul/'
+  },
   mode: 'universal',
   /*
   ** Headers of the page
